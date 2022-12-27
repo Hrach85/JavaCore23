9cbf811d08e4270f1b237daeaa6cac90b150e64a
Hrach85 <hrach9485@gmail.com>
2022-12-27 12:02:33 +0300

    homework 2.2

---
+++ src/chapter4/Modulus.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+public class Modulus {
+
+    public static void main(String[] args) {
+        System.out.println("\nApифмeтикa с плавающей точкой");
+        int x = 42;
+        double y = 42.25;
+
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("y mod 10 = " + y % 10);
+    }
+}
