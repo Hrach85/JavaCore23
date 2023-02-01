bd7e6f5bbea4398a9d923387e8dda1ad1e77933d
Hrach85 <hrach9485@gmail.com>
2023-02-01 22:41:14 +0300

    chapter 7

---
+++ src/chapter7/chapter7_9_1/Outer.java
@@ -0,0 +1,16 @@
+package chapter7.chapter7_9_1;
+
+public class Outer {
+    int outer_x = 100;
+
+    void Test() {
+        Inner inner = new Inner();
+        inner.display();
+    }
+
+    class Inner {
+        void display() {
+            System.out.println("вывoд: outer_x = " + outer_x);
+        }
+    }
+}
