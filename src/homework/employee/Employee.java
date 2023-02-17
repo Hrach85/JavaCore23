edb4312cf18f483341ceeecf2e9a9a285c95fc1e
Hrach85 <hrach9485@gmail.com>
2023-02-17 18:59:05 +0300

    Employee

--- src/homework/employee/Employee.java
+++ src/homework/employee/Employee.java
@@ -111,10 +111,6 @@ public class Employee {
         return registerDate;
     }
 
-    public void setRegisterDate(String registerDate) {
-        this.registerDate = registerDate;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
